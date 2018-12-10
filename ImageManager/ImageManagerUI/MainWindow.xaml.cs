cd1e0d6b1097fb83f3bc1d93da2088fc4e013bf6
Kenneth Coleman <l00144771@student.lyit.ie>
2018-12-10 21:38:02 +0000

    Framework added, added some stuff to login window.

--- ImageManager/ImageManagerUI/MainWindow.xaml.cs
+++ ImageManager/ImageManagerUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DBLibrary;
 
 namespace ImageManagerUI
 {
@@ -20,11 +21,32 @@ namespace ImageManagerUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        ImageManagementDBEntities db = new ImageManagementDBEntities("metadata=res://*/ImageAppModel.csdl|res://*/ImageAppModel.ssdl|res://*/ImageAppModel.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=192.168.81.100;initial catalog=ImageManagementDB;persist security info=True;user id=ImageManagement;password=Letmein101;pooling=False;MultipleActiveResultSets=True;App=EntityFramework&quot;");
         public MainWindow()
         {
-            ImageManagerDBEntities
+            
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string currentUser = txtUserName.Text;
+            string currentPassword = txtPassword.Password;
+            foreach (var user in db.Users)
+            {
+                if (user.Username == currentUser && user.Password == currentPassword)
+                {
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.ShowDialog();
+                    this.Hide();
+                }
+                else
+                {
+                    lvlErrorMessage.Content = "Username or password is incorrect";
+                }
+            }
+
+
         }
     }
 }
