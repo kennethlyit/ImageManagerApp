e9dac0f8e0c244cd358e5aeb94ce8f83cc0ea932
Kenneth Coleman <l00144771@student.lyit.ie>
2018-12-17 15:40:36 +0000

    Cleaned up new user registration

--- ImageManager/ImageManagerUI/Registration.xaml.cs
+++ ImageManager/ImageManagerUI/Registration.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DBLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,60 @@ namespace ImageManagerUI
     /// </summary>
     public partial class Registration : Window
     {
+
+        ImageManageEntities db = new ImageManageEntities("metadata=res://*/ImageManagerModel.csdl|res://*/ImageManagerModel.ssdl|res://*/ImageManagerModel.msl;provider=System.Data.SqlClient;provider connection string='data source=192.168.81.100;initial catalog=ImageManage;persist security info=True;user id=ImageManagement;password=Letmein101;MultipleActiveResultSets=True;App=EntityFramework'");
+
+        
+
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            user.Username = txtUserName.Text.Trim();
+            user.Password = txtPassword.Text.Trim();
+            user.Email = txtEmail.Text.Trim();
+            user.LevelID = 3;
+            int saveSuccess = SaveUser(user);
+            if (saveSuccess == 1)
+            {
+                MessageBox.Show("User added, please login", "Save to Database");
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                ClearUserDetails();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("User Not added", "Save to Database");
+            }
+
+        }
+
+        private void btnLoginCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            ClearUserDetails();
+            this.Hide();
+        }
+
+        private void ClearUserDetails()
+        {
+            txtEmail.Text = "";
+            txtUserName.Text = "";
+            txtPassword.Clear();
+            txtPassword2.Clear();
+        }
+
+        public int SaveUser(User user)
+        {
+            db.Entry(user).State = System.Data.Entity.EntityState.Added;
+            int saveSuccess = db.SaveChanges();
+            return saveSuccess;
         }
     }
 }
