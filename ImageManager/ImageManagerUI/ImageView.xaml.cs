0764076d9dfa24046b5bf4592557ef8bf2883ff8
Kenneth Coleman <l00144771@student.lyit.ie>
2018-12-20 23:09:56 +0000

    Building basic images pages

---
+++ ImageManager/ImageManagerUI/ImageView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageManagerUI
+{
+    /// <summary>
+    /// Interaction logic for ImageView.xaml
+    /// </summary>
+    public partial class ImageView : Page
+    {
+        public ImageView()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuAddImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnudeleteImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnumodifyImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuaddVendor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbousedIn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbovendor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnaddImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btncancelImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
