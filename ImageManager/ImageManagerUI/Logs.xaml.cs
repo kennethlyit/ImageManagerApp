78c93a84326f25182e9b586b746482fc0a62d99d
Kenneth Coleman <l00144771@student.lyit.ie>
2018-12-23 16:47:04 +0000

    Finished logs page, added try/catch and innerJoin for values

--- ImageManager/ImageManagerUI/Logs.xaml.cs
+++ ImageManager/ImageManagerUI/Logs.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DBLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,65 @@ namespace ImageManagerUI
     /// </summary>
     public partial class Logs : Page
     {
+
+        ImageManageEntities db = new ImageManageEntities("metadata=res://*/ImageManagerModel.csdl|res://*/ImageManagerModel.ssdl|res://*/ImageManagerModel.msl;provider=System.Data.SqlClient;provider connection string='data source=192.168.81.100;initial catalog=ImageManage;persist security info=True;user id=ImageManagement;password=Letmein101;MultipleActiveResultSets=True;App=EntityFramework'");
+
+        List<Log> logs = new List<Log>();
+        List<User> users = new List<User>();
+        
+
+
         public Logs()
         {
             InitializeComponent();
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this cause the list to populate on page load, might consider adding a rightclick refresh sub function if I have time
+            RefreshLogsList();
+        }
+
+        private void RefreshLogsList()
+        {
+            try
+            {
+                //Populates logs list
+                logs.Clear();
+                foreach (var log in db.Logs)
+                {
+                    logs.Add(log);
+                }
+                //populates users field
+                users.Clear();
+                foreach (var user in db.Users)
+                {
+                    users.Add(user);
+                }
+                //Does a inner join, creating joined values using the userID as the common point
+                var getUsername = from id in logs
+                                  join name in users on id.UserID equals name.UserID
+                                  select new
+                                  {
+                                      id.UserID,
+                                      id.Description,
+                                      id.Date,
+                                      userName = name.Username
+                                  };
+                //Tells the lst in XAML to use the new variable as the source
+                lstLogs.ItemsSource = getUsername;
+                //Populates it
+                lstLogs.Items.Refresh();
+            }
+            catch 
+            {
+                MessageBox.Show("Something went wrong with logs retrieval", "Logs Retrieval", MessageBoxButton.OK);
+            }
+            
+        }
+
+        
     }
+
+
 }
